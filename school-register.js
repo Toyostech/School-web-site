52714663e0bdcc855a1cc3ce6404d2ec8718f283
SALAMI TOYOSI <106937039+Toyostech@users.noreply.github.com>
2024-11-12 16:01:15 +0100

    F

--- school-register.js
+++ school-register.js
@@ -2,7 +2,7 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList
-    .toggle('window-scroll', window.scrolly > 0)
+    .toggle('window-scroll', window.scrollY > 0)
 })
 
 
